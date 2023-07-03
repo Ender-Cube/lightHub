3602af44aa77b8a343348890ae12e4f17e4a7d56
zax71 <zabax32@gmail.com>
2023-07-03 19:01:37 +0100

    Clean up command code

--- src/main/java/me/zax71/lightHub/Main.java
+++ src/main/java/me/zax71/lightHub/Main.java
@@ -46,8 +46,6 @@ public class Main {
     public static final Logger logger = LoggerFactory.getLogger(Main.class);
     public static Jedis REDIS;
 
-    private static LiteCommands<CommandSender> liteCommands;
-
 
     public static void main(String[] args) {
         initConfig();
@@ -107,7 +105,7 @@ public class Main {
     }
 
     private static void initCommands() {
-        liteCommands = LiteMinestomFactory.builder(MinecraftServer.getServer(), MinecraftServer.getCommandManager())
+        LiteMinestomFactory.builder(MinecraftServer.getServer(), MinecraftServer.getCommandManager())
                 // .commandInstance(new Command())
                 .argument(Player.class, new PlayerArgument(MinecraftServer.getServer()))
                 .register();
